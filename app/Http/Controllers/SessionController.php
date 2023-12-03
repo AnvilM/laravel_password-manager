8508f219586d0d18e98ae18820f39079acbfc8e7
AnvilM <211anvil112@gmail.com>
2023-12-03 20:29:53 +0300

    Add coments to SessionController

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -13,25 +13,32 @@ class SessionController extends Controller
      */
     public function index(Request $request)
     {
+
+        //Get token.
         $token = $request->header('token');
 
 
+        //Get token payload.
         $payload = SessionHelper::getTokenPayload($token);
 
+        //Get account id from token payload.
         $account_id = $payload['id'];
 
 
+        //Get session from account id.
         $Session = new SessionModel();
 
         $Session = $Session->where('account_id', $account_id)->get();
 
 
+        //Checks if sessions exists.
         if ($Session->count() == 0)
         {
             return response('Invalid account_id', 422);
         }
 
 
+        //Return sessions.
         return response()->json($Session->toArray());
     }
 
@@ -40,27 +47,34 @@ class SessionController extends Controller
      */
     public function show(Request $request)
     {
+        //Get token.
         $token = $request->header('token');
 
+        //Get session id.
         $id = $request->route('id');
 
 
+        //Get payload from token.
         $payload = SessionHelper::getTokenPayload($token);
 
+        //Get account id from token payload.
         $account_id = $payload['id'];
 
 
+        //Get session from session id and account id.
         $Session = new SessionModel();
 
         $Session = $Session->where('id', $id)->where('account_id', $account_id)->get();
 
 
+        //Checks if session exists.
         if ($Session->count() == 0)
         {
             return response('Invalid id', 422);
         }
 
 
+        //Return session.
         return response()->json($Session->toArray());
     }
 
@@ -79,19 +93,19 @@ class SessionController extends Controller
         //Get account id from token payload data.
         $account_id = $payload['id'];
 
-        //Get password id.
+        //Get session id.
         $id = $request->route('id');
 
 
         $Session = new SessionModel();
 
-        //Checks if password exists.
+        //Checks if session exists.
         if ($Session->where('id', $id)->where('account_id', $account_id)->count() == 0)
         {
             return response('Invalid id', 422);
         }
 
-        //Delete password.
+        //Delete session.
         $Session->where('id', $id)->where('account_id', $account_id)->delete();
     }
 }
